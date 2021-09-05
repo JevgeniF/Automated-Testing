e0b3235040e8e087f713ccc7038dca64c05e4a75
Jevgeni Fenko <jefenk@taltech.ee>
2021-09-05 16:12:31 +0300

    Code updated for Requirement 4

--- assignment1_tdd_kata/src/Greeting.java
+++ assignment1_tdd_kata/src/Greeting.java
@@ -5,4 +5,8 @@ public class Greeting {
         if (name.equals(name.toUpperCase())) return String.format("HELLO, %s!", name);
         return String.format("Hello, %s.", name);
     }
+
+    public static String greet(String[] name) {
+        return String.format("Hello, %s and %s", name[0], name[1]);
+    }
 }
