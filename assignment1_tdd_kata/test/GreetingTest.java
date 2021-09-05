8dd52c79ad42b8cdca8032f32da84f569fb09e18
Jevgeni Fenko <jefenk@taltech.ee>
2021-09-05 16:09:51 +0300

    test TwoNamesArrayHandling added for Requirement 4. testNullStringHandling code updated.

--- assignment1_tdd_kata/test/GreetingTest.java
+++ assignment1_tdd_kata/test/GreetingTest.java
@@ -11,12 +11,19 @@ public class GreetingTest {
 
     @Test   // Requirement 2. The method handles nulls and returns greeting.
     public void testNullStringHandling() {
-        Assert.assertEquals("Hello, my friend.", Greeting.greet(null));
+        String name = null;
+        Assert.assertEquals("Hello, my friend.", Greeting.greet(name));
     }
 
     @Test   // Requirement 3. The method returns greeting all uppercase if name is all uppercase.
     public void testUppercaseGreetingIfNameUppercase() {
         String name = "JERRY";
         Assert.assertEquals("HELLO, JERRY!", Greeting.greet(name));
+    }
+
+    @Test   // Requirement 4. The method returns greeting for all persons listed in array of two names.
+    public void testTwoNamesArrayHandling() {
+        String[] name = new String[] {"Jill", "Jane"};
+        Assert.assertEquals("Hello, Jill and Jane", Greeting.greet(name));
     }
 }
