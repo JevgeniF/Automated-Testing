fa44e161ab41dfea0d3a50de05719383b7ef0e0a
Jevgeni Fenko <jefenk@taltech.ee>
2021-09-05 15:35:55 +0300

    Code updated for Requirement 2

--- assignment1_tdd_kata/src/Greeting.java
+++ assignment1_tdd_kata/src/Greeting.java
@@ -1,6 +1,9 @@
 public class Greeting {
 
     public static String greet(String name) {
+        if (name == null) {
+            return "Hello, my friend.";
+        }
         return String.format("Hello, %s.", name);
     }
 }
