e5708e78f9ff1c180a78f5cc9ac215358129556c
Jevgeni Fenko <jefenk@taltech.ee>
2021-09-05 16:43:30 +0300

    Code updated for Requirement 5

--- assignment1_tdd_kata/src/Greeting.java
+++ assignment1_tdd_kata/src/Greeting.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Greeting {
 
     public static String greet(String name) {
@@ -7,6 +10,13 @@ public class Greeting {
     }
 
     public static String greet(String[] name) {
-        return String.format("Hello, %s and %s", name[0], name[1]);
+        if (name.length == 2) return String.format("Hello, %s and %s.", name[0], name[1]);
+        List<String> allNamesExcLast = new ArrayList<>();
+        for (int i = 0; i < name.length - 1; i++) {
+            allNamesExcLast.add(name[i]);
+        }
+        String lastName = name[name.length - 1];
+        String firstNamesInGreeting = String.join(", ", allNamesExcLast);
+        return String.format("Hello, %s, and %s.", firstNamesInGreeting, lastName);
     }
 }
