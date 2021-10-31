172620ba7b0c11fbf29d141f13d6a21aa6460be0
Jevgeni <jefenk@taltech.ee>
2021-10-31 22:28:24 +0200

    Selenide Test. shouldDisplayViewProfileWhenHoveringOverSecondProfilePicture

--- ui-automation-selenide/src/test/java/icd0004/tests_selenide_only/HoverPageTests.java
+++ ui-automation-selenide/src/test/java/icd0004/tests_selenide_only/HoverPageTests.java
@@ -49,6 +49,11 @@ public class HoverPageTests {
         $(hoversOnPage.get(1)).hover().$("h5").shouldHave(text("name: user2"));
     }
 
+    @Test //Hovers over second profile picture and checks if page "View profile" link appears.
+    public void shouldDisplayViewProfileWhenHoveringOverSecondProfilePicture() {
+        $(hoversOnPage.get(1)).hover().$("a").shouldHave(text("View profile"));
+    }
+
     @Test //Hovers over third profile picture and checks if text field "name: user3" appears.
     public void shouldDisplayUsersNameWhenHoveringOverThirdProfilePicture() {
         $(hoversOnPage.get(2)).hover().$("h5").shouldHave(text("name: user3"));
