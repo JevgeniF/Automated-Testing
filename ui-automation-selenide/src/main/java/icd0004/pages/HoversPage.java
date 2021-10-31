5b939829c58eb8685f2c74b3b8b85104d1425b7f
Jevgeni <jefenk@taltech.ee>
2021-11-01 01:07:48 +0200

    Selenide POM Test. HoversPage object methods updated

--- ui-automation-selenide/src/main/java/icd0004/pages/HoversPage.java
+++ ui-automation-selenide/src/main/java/icd0004/pages/HoversPage.java
@@ -1,19 +1,32 @@
 package icd0004.pages;
 
+import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class HoversPage {
-    private final By hoverPageLink = By.linkText("Hovers");
+    private final By hoversPageLink = By.linkText("Hovers");
+    private final By nameField = By.cssSelector("h5");
+    private static List<WebElement> figureClassElementsList;
 
     public void goTo() {
         open("/");
-        $(hoverPageLink).click();
+        $(hoversPageLink).click();
+        figureClassElementsList = WebDriverRunner.getWebDriver().findElements(By.className("figure"));
     }
 
     public String getPageTitleText()  {
         return $("h3").getText();
     }
+
+    public String getHoveredProfileNameFieldText(int index) {
+       return $(figureClassElementsList.get(index)).hover().$("h5").getText();
+    }
+
 }
