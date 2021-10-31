62e2ffea0c8c806ae2f076b2449a44376759eb80
Jevgeni <jefenk@taltech.ee>
2021-11-01 01:11:45 +0200

    Selenide POM Test. shouldDisplayUsersNameWhenHoveringOverThirdProfilePicture

--- ui-automation-selenide/src/test/java/icd0004/tests_selenide_with_pom/HoversPageTests.java
+++ ui-automation-selenide/src/test/java/icd0004/tests_selenide_with_pom/HoversPageTests.java
@@ -4,6 +4,8 @@ import icd0004.pages.HoversPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HoversPageTests extends BaseTest {
@@ -29,5 +31,10 @@ public class HoversPageTests extends BaseTest {
     @Test //Hovers over second profile picture and checks if text field "name: user2" appears.
     public void shouldDisplayUsersNameWhenHoveringOverSecondProfilePicture() {
         assertThat(hoversPage.getHoveredProfileNameFieldText(1)).isEqualTo("name: user2");
+    }
+
+    @Test //Hovers over third profile picture and checks if text field "name: user3" appears.
+    public void shouldDisplayUsersNameWhenHoveringOverThirdProfilePicture() {
+        assertThat(hoversPage.getHoveredProfileNameFieldText(2)).isEqualTo("name: user3");
     }
 }
