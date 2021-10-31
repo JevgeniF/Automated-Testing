411b8000cc88d54ddf8868fc4eb57ccb35fb1398
Jevgeni <jefenk@taltech.ee>
2021-10-31 22:19:33 +0200

    Selenide Test. shouldDisplayUsersNameWhenHoveringOverFirstProfilePicture

--- ui-automation-selenide/src/test/java/icd0004/tests_selenide_only/HoverPageTests.java
+++ ui-automation-selenide/src/test/java/icd0004/tests_selenide_only/HoverPageTests.java
@@ -1,15 +1,23 @@
 package icd0004.tests_selenide_only;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class HoverPageTests {
+
+    private static List<WebElement> hoversOnPage;
+
     @BeforeAll
     public static void setUp() {
         Configuration.baseUrl = "https://the-internet.herokuapp.com";
@@ -18,10 +26,16 @@ public class HoverPageTests {
 
         open("/");
         $(By.linkText("Hovers")).click();
+        hoversOnPage = WebDriverRunner.getWebDriver().findElements(By.className("figure"));
     }
 
     @Test //Opens "Hovers" page by click on link and checks if page name matches.
     public void canOpenHoversPage() {
         $("h3").shouldHave(text("Hovers"));
+    }
+
+    @Test //Hovers over first profile picture and checks if page "name" field text matches.
+    public void shouldDisplayUsersNameWhenHoveringOverFirstProfilePicture() {
+        $(hoversOnPage.get(0)).hover().$("h5").shouldHave(text("name: user1"));
     }
 }
