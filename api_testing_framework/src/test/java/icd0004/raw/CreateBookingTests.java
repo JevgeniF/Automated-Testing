b6287492242021135807d9fdf1dc867c59d82580
Jevgeni Fenko <jefenk@taltech.ee>
2021-11-01 12:52:16 +0200

    Raw CreateBookingTests. postBookingWithWrongAcceptHeaderShouldReturnImATeapotError

--- api_testing_framework/src/test/java/icd0004/raw/CreateBookingTests.java
+++ api_testing_framework/src/test/java/icd0004/raw/CreateBookingTests.java
@@ -61,4 +61,32 @@ public class CreateBookingTests {
                 .body("booking.firstname", equalTo("Jevgeni"))
                 .body("booking.lastname", equalTo("Fenko"));
     }
+
+    @Test //Posts booking with wrong "Accept" header and checks if API returns status code 418
+    public void postBookingWithWrongAcceptHeaderShouldReturnImATeapotError() {
+        String acceptHeader = "application/pdf";
+
+        String payload = """
+                {
+                    "firstname": "Jevgeni",
+                    "lastname": "Fenko",
+                    "totalprice": 12,
+                    "depositpaid": true,
+                    "bookingdates": {
+                        "checkin": "2021-10-30",
+                        "checkout": "2021-10-31"
+                    }
+                }""";
+
+        given()
+                .accept(JSON.toString())
+                .contentType(JSON.toString())
+                .header("Accept", acceptHeader)
+                .body(payload)
+                .when()
+                .post(API_URL)
+                .then()
+                .statusCode(418);
+    }
+
 }
