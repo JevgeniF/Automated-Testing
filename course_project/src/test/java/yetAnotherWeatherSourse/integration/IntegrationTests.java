d1ba7c711b4734df4bdec405578ae04e8a3891f1
Jevgeni <jefenk@taltech.ee>
2021-11-01 22:16:09 +0200

    Weather Api Integration tests. ShouldReturnHttpOkWhenCityNameIsGiven

---
+++ course_project/src/test/java/yetAnotherWeatherSourse/integration/IntegrationTests.java
@@ -0,0 +1,19 @@
+package yetAnotherWeatherSourse.integration;
+
+import org.junit.jupiter.api.Test;
+import yetAnotherWeatherSource.api.WeatherApi;
+
+import static java.net.HttpURLConnection.HTTP_OK;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IntegrationTests {
+
+    @Test
+    public void ShouldReturnHttpOkWhenCityNameIsGiven() {
+        String city = "Haabneeme";
+
+        int RequestStatus = WeatherApi.getCurrentWeatherResponse(city).getStatus();
+        assertThat(RequestStatus).isEqualTo(HTTP_OK);
+    }
+
+}
