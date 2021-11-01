6935bb360e811ca7a18c68d2672d2f69a6dd120b
Jevgeni Fenko <jefenk@taltech.ee>
2021-11-01 16:46:03 +0200

    Framework Tests. postAuthenticationWithWrongCredentialsShouldReturnBadCredentialsMessage

--- api_testing_framework/src/test/java/icd0004/framework/AuthenticationTests.java
+++ api_testing_framework/src/test/java/icd0004/framework/AuthenticationTests.java
@@ -1,6 +1,7 @@
 package icd0004.framework;
 
 import icd0004.framework.request.Authentication;
+import icd0004.framework.response.AuthenticationResponse;
 import org.junit.jupiter.api.Test;
 
 import static java.net.HttpURLConnection.HTTP_OK;
@@ -17,5 +18,16 @@ public class AuthenticationTests {
                 .postAuthentication(authenticationCredentials).getStatusCode();
 
         assertThat(statusCode).isEqualTo(HTTP_OK);
+    }
+
+    @Test //Posts wrong credentials and checks if API returns "Bad credentials" message in body.
+    public void postAuthenticationWithWrongCredentialsShouldReturnBadCredentialsMessage() {
+        Authentication authenticationCredentials = Authentication.getGeneratedCredentials();
+
+        AuthenticationResponse authenticationResponse = AuthenticationApi
+                .postAuthentication(authenticationCredentials)
+                .as(AuthenticationResponse.class);
+
+        assertThat(authenticationResponse.getReason()).isEqualTo("Bad credentials");
     }
 }
