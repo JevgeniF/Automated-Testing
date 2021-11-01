be7cafdd7369dfd6c8ca58eff5c943c093a0d9dd
Jevgeni <jefenk@taltech.ee>
2021-11-01 23:25:44 +0200

    CurrentWeatherData update with CoordinatesModel

---
+++ course_project/src/main/java/yetAnotherWeatherSource/api/model/CoordinatesModel.java
@@ -0,0 +1,9 @@
+package yetAnotherWeatherSource.api.model;
+
+import lombok.Data;
+
+@Data
+public class CoordinatesModel {
+    private Double lon;
+    private Double lat;
+}
--- course_project/src/main/java/yetAnotherWeatherSource/api/response/CurrentWeatherData.java
+++ course_project/src/main/java/yetAnotherWeatherSource/api/response/CurrentWeatherData.java
@@ -2,9 +2,11 @@ package yetAnotherWeatherSource.api.response;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import lombok.Data;
+import yetAnotherWeatherSource.api.model.CoordinatesModel;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CurrentWeatherData {
     private String name;
+    private CoordinatesModel coord;
 }
