b1ed2ea2ac1fffdec832b53ceb014b47e977e826
Jevgeni <jefenk@taltech.ee>
2021-11-01 23:20:41 +0200

    CurrentWeatherIntegrationTests test ShouldReturnSameCityNameInWeatherReportDetails

--- course_project/src/test/java/yetAnotherWeatherSourse/integration/CurrentWeatherIntegrationTests.java
+++ course_project/src/test/java/yetAnotherWeatherSourse/integration/CurrentWeatherIntegrationTests.java
@@ -2,6 +2,7 @@ package yetAnotherWeatherSourse.integration;
 
 import org.junit.jupiter.api.Test;
 import yetAnotherWeatherSource.api.WeatherApi;
+import yetAnotherWeatherSource.api.response.CurrentWeatherData;
 
 import static java.net.HttpURLConnection.HTTP_OK;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,5 +15,13 @@ public class CurrentWeatherIntegrationTests {
 
         int RequestStatus = WeatherApi.getCurrentWeatherResponse(city).getStatus();
         assertThat(RequestStatus).isEqualTo(HTTP_OK);
+    }
+
+    @Test
+    public void ShouldReturnSameCityNameInWeatherReportDetails() {
+        String city = "Haabneeme";
+        CurrentWeatherData currentWeatherData = WeatherApi.getCurrentWeatherData(city);
+
+        assertThat(currentWeatherData.getName()).isEqualTo(city);
     }
 }
