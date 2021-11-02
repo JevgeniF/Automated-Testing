d8224ab2fb7b4db91c37411b568941687a872cb7
Jevgeni <jefenk@taltech.ee>
2021-11-02 15:37:54 +0200

    CurrentWeatherInAppFunctionalityTests test inAppWeatherReportShouldHaveTemperature

--- course_project/src/test/java/yetAnotherWeatherSourse/appFunctionality/CurrentWeatherInAppFunctionalityTests.java
+++ course_project/src/test/java/yetAnotherWeatherSourse/appFunctionality/CurrentWeatherInAppFunctionalityTests.java
@@ -50,6 +50,12 @@ public class CurrentWeatherInAppFunctionalityTests {
     }
 
     @Test
+    public void inAppWeatherReportShouldHaveTemperature() throws CityNotFoundException {
+        WeatherReport weatherReport = yetAnotherWeatherSource.getWeatherReport(city);
+        assertThat(weatherReport.getCurrentWeather().getTemperature()).isNotNull();
+    }
+
+    @Test
     public void inAppWeatherReportCoordinatesInFormat_LatLon() throws CityNotFoundException {
         CurrentWeatherData currentWeatherData = weatherApi.getCurrentWeatherData(city);
         WeatherReport weatherReport = yetAnotherWeatherSource.getWeatherReport(city);
