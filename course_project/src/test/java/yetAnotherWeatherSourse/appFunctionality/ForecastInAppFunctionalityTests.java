35eb7dbae42cc49884865b07a89bc5a4cd49c9f2
Jevgeni <jefenk@taltech.ee>
2021-11-03 00:51:00 +0200

    ForecastInAppFunctionalityTests test inAppWeatherReportShouldHaveSectionForecastReport

---
+++ course_project/src/test/java/yetAnotherWeatherSourse/appFunctionality/ForecastInAppFunctionalityTests.java
@@ -0,0 +1,31 @@
+package yetAnotherWeatherSourse.appFunctionality;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import yetAnotherWeatherSource.YetAnotherWeatherSource;
+import yetAnotherWeatherSource.api.WeatherApi;
+import yetAnotherWeatherSource.api.exception.CityNotFoundException;
+import yetAnotherWeatherSource.model.WeatherReport;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ForecastInAppFunctionalityTests {
+    private static String city;
+    private static WeatherApi weatherApi;
+    private static YetAnotherWeatherSource yetAnotherWeatherSource;
+
+    @BeforeAll // initial setup for all tests
+    static void setUp() {
+        city = "Florence";
+        weatherApi = new WeatherApi();
+        yetAnotherWeatherSource = new YetAnotherWeatherSource(weatherApi);
+    }
+
+    //----------------------- STRUCTURE TESTS -----------------------//
+
+    @Test //Checks if weather report has section with forecast report
+    public void inAppWeatherReportShouldHaveSectionForecastReport() throws CityNotFoundException {
+        WeatherReport weatherReport = yetAnotherWeatherSource.getWeatherReport(city);
+        assertThat(weatherReport.getForecastReport()).isNotNull();
+    }
+}
