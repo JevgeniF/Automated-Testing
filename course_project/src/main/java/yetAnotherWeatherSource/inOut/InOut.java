6c34fdd7e503b9472236f81ecc1d6be46a32749e
Jevgeni <jefenk@taltech.ee>
2021-11-03 16:36:59 +0200

    InOut method to save json file added. got some mess in IDE with resources folder. Recreated.

--- course_project/src/main/java/yetAnotherWeatherSource/inOut/InOut.java
+++ course_project/src/main/java/yetAnotherWeatherSource/inOut/InOut.java
@@ -1,9 +1,12 @@
 package yetAnotherWeatherSource.inOut;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import yetAnotherWeatherSource.exception.FileInputMissingException;
 import yetAnotherWeatherSource.exception.FileNotFoundException;
 import yetAnotherWeatherSource.exception.WrongInputFormatException;
+import yetAnotherWeatherSource.model.WeatherReport;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -26,6 +29,18 @@ public class InOut {
             return Files.readString(fileLocation).trim();
         } catch (IOException e) {
             throw new FileNotFoundException();
+        }
+    }
+
+    public static void saveToJson(String fileLocationPath, WeatherReport weatherReport) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.writeValue(new File(fileLocationPath + "Weather in " +
+                            weatherReport.getWeatherReportDetails().getCity() +
+                            ".json"),
+                    weatherReport);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- course_project/src/main/java/yetAnotherWeatherSource/model/WeatherReport.java
+++ course_project/src/main/java/yetAnotherWeatherSource/model/WeatherReport.java
@@ -1,10 +1,7 @@
 package yetAnotherWeatherSource.model;
 
 import lombok.Data;
-import org.codehaus.jackson.map.ObjectMapper;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -19,17 +16,4 @@ public class WeatherReport {
     private CurrentWeatherReport currentWeatherReport;
     private ArrayList<ForecastReport> forecastReport;
 
-    /**
-     * Method saves class entity as .json file
-     */
-    @SuppressWarnings("unused")
-    public void toJSON() {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            mapper.writeValue(new File("./Weather in " + weatherReportDetails.getCity() + ".json"),
-                    this);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
