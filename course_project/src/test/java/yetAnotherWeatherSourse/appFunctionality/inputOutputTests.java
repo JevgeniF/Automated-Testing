5edd36102cb47d4db155fdeb76aaa8c5bd4bf8dd
Jevgeni <jefenk@taltech.ee>
2021-11-03 14:40:27 +0200

    InputOutputTests test inAppThrowsWrongInputFormatExceptionIfFileHasWrongFormat

--- course_project/src/test/java/yetAnotherWeatherSourse/appFunctionality/inputOutputTests.java
+++ course_project/src/test/java/yetAnotherWeatherSourse/appFunctionality/inputOutputTests.java
@@ -11,10 +11,12 @@ import yetAnotherWeatherSource.model.WeatherReport;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class inputOutputTests {
     private static WeatherApi weatherApi;
     private static YetAnotherWeatherSource yetAnotherWeatherSource;
+    private static final String INPUT_DATA_FOLDER = "./src/main/resources/test_data/Input/";
 
     @BeforeAll
     public static void setUp() {
@@ -24,11 +26,21 @@ public class inputOutputTests {
 
     @Test
     public void inAppGetWeatherReportForCityFromFile() throws CityNotFoundException, IOException {
-        String city = InOut.getCityFromFile("./src/main/resources/test_data/Input/city.txt");
+        String city = InOut.getCityFromFile(INPUT_DATA_FOLDER + "city.txt");
 
         WeatherReport weatherReport = yetAnotherWeatherSource.getWeatherReport(city);
 
         assertThat(weatherReport.getWeatherReportDetails().getCity()).isEqualTo("Plymouth");
+
+    }
+
+    @Test
+    public void inAppThrowsWrongInputFormatExceptionIfFileHasWrongFormat() throws CityNotFoundException, IOException {
+        String exceptionErrorMessage = "Wrong file format. Should be txt file.";
+        Exception exception = assertThrows(WrongInputFormatException.class, () ->
+                InOut.getCityFromFile(INPUT_DATA_FOLDER + "wrong_format.pdf"));
+
+        assertThat(exception.getMessage()).isEqualTo(exceptionErrorMessage);
 
     }
 }
