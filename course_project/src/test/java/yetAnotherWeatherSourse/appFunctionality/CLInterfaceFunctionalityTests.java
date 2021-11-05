2a633528d0f75da21e2c4974bd5872871f95e2ea
Jevgeni <jefenk@taltech.ee>
2021-11-05 12:38:42 +0200

    CLInterfaceFunctionalityTests test interfaceShouldOutputWeatherReportToConsoleIfArgsConsoleAndCityGiven

---
+++ course_project/src/test/java/yetAnotherWeatherSourse/appFunctionality/CLInterfaceFunctionalityTests.java
@@ -0,0 +1,53 @@
+package yetAnotherWeatherSourse.appFunctionality;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import yetAnotherWeatherSource.CLInterface;
+import yetAnotherWeatherSource.YetAnotherWeatherSource;
+import yetAnotherWeatherSource.api.WeatherApi;
+import yetAnotherWeatherSource.exception.CityNotFoundException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CLInterfaceFunctionalityTests {
+
+    private PrintStream printStream;
+    public static YetAnotherWeatherSource yetAnotherWeatherSource;
+    private PrintStream oldPrintStream;
+    private ByteArrayOutputStream outputStream;
+
+    @BeforeAll
+    static void setUp() {
+        WeatherApi weatherApi = new WeatherApi();
+        yetAnotherWeatherSource = new YetAnotherWeatherSource(weatherApi);
+    }
+
+    @BeforeEach
+    void streamSetUp() {
+        outputStream = new ByteArrayOutputStream();
+        printStream = new PrintStream(outputStream);
+        oldPrintStream = System.out;
+    }
+
+    @AfterEach
+    void streamRestore() {
+        System.out.flush();
+        System.setOut(oldPrintStream);
+    }
+
+    @Test
+    public void interfaceShouldOutputWeatherReportToConsoleIfArgsConsoleAndCityGiven() throws CityNotFoundException {
+        String cityAsString = "Alabama";
+        System.setOut(printStream);
+        CLInterface.main(new String[] {"-console", cityAsString});
+
+        assertThat(outputStream.toString())
+                .isEqualTo(yetAnotherWeatherSource.getWeatherReport("Alabama").toString());
+
+    }
+}
